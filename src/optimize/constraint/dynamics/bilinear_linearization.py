ede1a3ef6bc71660c43cd1716b17c2e9f1e15859
Alex Brown <42049885+alexb7711@users.noreply.github.com>
2022-12-21 15:06:24 +0000

    Update constraints

--- src/optimize/constraint/dynamics/bilinear_linearization.py
+++ src/optimize/constraint/dynamics/bilinear_linearization.py
@@ -19,6 +19,7 @@ class BilinearLinearization(Constraint):
 		# Extract parameters
 		M   = self.params['T']
 		Q   = self.params['Q']
+		G   = self.params['Gamma']
 		gam = self.params['gamma']
 
 		# Extract decision vars
@@ -28,8 +29,8 @@ class BilinearLinearization(Constraint):
 
 		if gam[i] >= 0:
 			for q in range(Q):
-				model.addConstr(g[i][q] <= p[i]			  , name="{0}_{1}".format(self.name,i  ))
-				model.addConstr(g[i][q] >= p[i] - (1 - w[i][q])*M , name="{0}_{1}".format(self.name,i+1))
-				model.addConstr(g[i][q] <= M*w[i][q]		  , name="{0}_{1}".format(self.name,i+2))
-				model.addConstr(g[i][q] >= 0			  , name="{0}_{1}".format(self.name,i+3))
+				model.addConstr(g[i][q] <= p[i]		        	  , name="{0}_{1}_{2}".format(self.name,G[i],i  ))
+				model.addConstr(g[i][q] >= p[i] - (1 - w[i][q])*M , name="{0}_{1}_{2}".format(self.name,G[i],i+1))
+				model.addConstr(g[i][q] <= M*w[i][q]		      , name="{0}_{1}_{2}".format(self.name,G[i],i+2))
+				model.addConstr(g[i][q] >= 0			          , name="{0}_{1}_{2}".format(self.name,G[i],i+3))
 		return
