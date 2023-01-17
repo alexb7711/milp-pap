772819db56033d9a409e9cdbe2a0407e26c42fb9
Alexander Brown <alex.brown7711@gmail.com>
2023-01-17 13:35:07 -0700

    Added an epsilon for scheduleOuput

--- src/util/data_output.py
+++ src/util/data_output.py
@@ -248,7 +248,7 @@ def __scheduleOut(fn,dm,path):
 
     # For each visit
     for i in range(N):
-        if g[i][v[i]] > 0:
+        if g[i][v[i]] > 0.001:
             data[i][G[i]*3+0] = v[i]
             data[i][G[i]*3+1] = u[i]
             data[i][G[i]*3+2] = g[i][v[i]]
